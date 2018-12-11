5367c26422c5e89eac087837f280fd4484f3bd88
Yanfeng Liu <yanfengliux@gmail.com>
2018-12-11 15:52:45 -0600

    add comment for matlab file; display line as soon as user starts drawing polygons

--- getResults.m
+++ getResults.m
@@ -1,15 +1,24 @@
 clear all;
 clc;
 
+% read table
 T = readtable('sample_result.csv','Delimiter',',','ReadVariableNames',false);
+
+% get class name, img url, and annotation JSON struct
 class_names = strjoin(cellstr(table2cell(T(1, 30))));
 class_names = split(class_names, '-');
 class_num = length(class_names);
 img_url = strjoin(cellstr(table2cell(T(1, 28))));
 ann = jsondecode(strjoin(cellstr(table2cell(T(1, 31)))));
+
+% download image from img url
 option = weboptions('Timeout', 10);
 img = webread(img_url);
+
+% display image
 figure; hold on;
+
+% calculate the ratio between original image and the one displayed on Amazon MTurk
 ratio = size(img, 2)/1000;
 % hash class names and generate repeatable class colors
 colors = ones([class_num, 3]);
@@ -20,6 +29,9 @@ for i = 1:class_num
    hsv = [hue, 1, 1];
    colors(i, :) = hsv2rgb(hsv);
 end
+
+% display annotations as filled transparent polygons. Only supports polygons right now. 
+% TODO: Add support for dots and links. 
 for i = 1:size(ann, 1)
     if (strcmp(ann(i).mode, 'polygon'))
         coordinates = [];
