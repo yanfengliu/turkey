83741c4fdf3f372a85f11eeb381fa91c1fefbe09
Yanfeng Liu <yanfengliux@gmail.com>
2018-12-13 14:17:57 -0600

    merge myHash.m into getResults.m

--- getResults.m
+++ getResults.m
@@ -49,3 +49,13 @@ for i = 1:size(ann, 1)
 end
 legend('soybean', 'corn', 'palmer');
 imshow(img);
+
+function [val] = myHash(input_string)
+   out_string = mlreportgen.utils.hash(input_string);
+   hex_string = dec2hex(char(out_string));
+   whole_hex_string = "";
+   for i = 2:4
+       whole_hex_string = strcat(whole_hex_string, hex_string(i, :));
+   end
+   val = str2double(whole_hex_string);
+end
--- myHash.m
+++
@@ -1,9 +0,0 @@
-function [val] = myHash(input_string)
-    out_string = mlreportgen.utils.hash(input_string);
-    hex_string = dec2hex(char(out_string));
-    whole_hex_string = "";
-    for i = 2:4
-        whole_hex_string = strcat(whole_hex_string, hex_string(i, :));
-    end
-    val = str2double(whole_hex_string);
-end
