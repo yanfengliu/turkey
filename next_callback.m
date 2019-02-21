5e04f5b249a41052647ff912be16f89cb07dcf77
Yanfeng Liu <yanfengliux@gmail.com>
2019-02-20 22:37:32 -0600

    working: finish_and_export

--- next_callback.m
+++
@@ -1,9 +0,0 @@
-function next_callback(hObject, eventdata, handles)
-    % get data from figure
-    data = guidata(hObject);
-    T = data.result_table;
-    row = data.row;
-    row = row + 1;
-    show_ann(T, row);
-    guidata(hObject, struct('row', row, 'result_table', T));
-end
